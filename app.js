376a38e786017794f85d3ecdd4fdb670f2872fab
haithem00gouder <gouderfifa21@gmail.com>
2023-11-01 18:35:11 +0100

    update

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: "dxxnd2npq",
+  api_key: "851758386114419",
+  api_secret: "N15mWYQ9NElGipAgyApsiTcae38",
+});
 require("dotenv").config();
 app.use(express.json());
 app.use(cookieParser());
@@ -12,7 +18,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-//upload image
+/*upload image
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "../front-end/clientside/public/upload");
@@ -27,6 +33,31 @@ const upload = multer({ storage });
 app.post("/api/upload", upload.single("file"), function (req, res) {
   const file = req.file;
   res.status(200).json(file.filename);
+});
+
+*/
+async function handleUpload(file) {
+  const res = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return res;
+}
+const storage = new multer.memoryStorage();
+const upload = multer({
+  storage,
+});
+app.post("/api/upload", upload.single("my_file"), async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    const cldRes = await handleUpload(dataURI);
+    res.json(cldRes);
+  } catch (error) {
+    console.log(error);
+    res.send({
+      message: error.message,
+    });
+  }
 });
 
 //routes
